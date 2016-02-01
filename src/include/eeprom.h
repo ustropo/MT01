41ecf41d548db1740062a1994e913238cd01a7b3
leocafonso <leocafonso@gmail.com>
2016-02-01 18:53:26 -0200

    Adicionado o visor de velocidade no menu manual

---
+++ src/include/eeprom.h
@@ -0,0 +1,21 @@
+/*
+ * eeprom.h
+ *
+ *  Created on: 31/01/2016
+ *      Author: leocafonso
+ */
+
+#ifndef INCLUDE_EEPROM_H_
+#define INCLUDE_EEPROM_H_
+
+extern uint8_t alturaDeslocamento;
+extern uint8_t alturaPerfuracao;
+extern uint8_t tempoPerfuracao;
+extern uint8_t alturaCorte;
+extern uint8_t velocidadeCorte;
+extern uint16_t jogLento;
+extern uint16_t jogRapido;
+extern uint8_t bIHS;
+
+
+#endif /* INCLUDE_EEPROM_H_ */
